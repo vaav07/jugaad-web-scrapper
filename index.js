b8029f1bc5369e013ab68f6f0aa87db3353f0fad
vaav07 <v071997@gmail.com>
2024-06-28 16:48:56 +0530

    fixess required

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // index.js
 const axios = require("axios");
 const XLSX = require("xlsx");
+const fs = require("fs");
 // require('dotenv').config();
 
 const pageNumber = 1;
+const pageSize = 25;
+// const letter = "o"
 
-const API_URL = `https://api.messefrankfurt.com/service/esb_api/exhibitor-service/api/2.1/public/exhibitor/search?language=en-GB&q=&orderBy=name&pageNumber=${1}&pageSize=10&orSearchFallback=false&showJumpLabels=false&jumpLabelId=n&findEventVariable=HEIMTEXTIL`;
+const API_URL = `https://api.messefrankfurt.com/service/esb_api/exhibitor-service/api/2.1/public/exhibitor/search?language=en-GB&q=&orderBy=name&pageNumber=${pageNumber}&pageSize=${pageSize}&orSearchFallback=false&showJumpLabels=false&jumpLabelId=o&findEventVariable=HEIMTEXTIL`;
 
 const fetchData = async () => {
   try {
@@ -27,37 +30,19 @@ const removeHtmlTags = (str) => {
   return str.replace(/<br \/>/g, " ").trim();
 };
 
-// const filterData = (data) => {
-//   return data.map((item) => {
-//     const exhibitor = item.exhibitor;
-//     const exhibitionHall = exhibitor.exhibition.exhibitionHall.length
-//       ? exhibitor.exhibition.exhibitionHall[0]
-//       : {};
-//     const stand = exhibitionHall.stand.length ? exhibitionHall.stand[0] : {};
-//     const contacts = exhibitor.contacts.map((contact) => ({
-//       firstname: contact.firstname,
-//       lastname: contact.lastname,
-//       department: contact.department,
-//       position: contact.position,
-//       phone: contact.phone,
-//       mobile: contact.mobile,
-//       email: contact.email,
-//     }));
+const removeDuplicates = (data, key) => {
+  const uniqueData = [];
+  const emailSet = new Set();
 
-//     return {
-//       name: exhibitor.name,
-//       country: exhibitor.address.country.label,
-//       email: exhibitor.address.email,
-//       href: exhibitor.href,
-//       homepage: exhibitor.homepage,
-//       tel: exhibitor.address.tel,
-//       formatedAddress: exhibitor.addressrdm.formatedAddress,
-//       contacts: contacts,
-//       hallAndLevel: exhibitionHall.id,
-//       firstBoothNumber: stand.name,
-//     };
-//   });
-// };
+  data.forEach((item) => {
+    if (!emailSet.has(item[key])) {
+      emailSet.add(item[key]);
+      uniqueData.push(item);
+    }
+  });
+
+  return uniqueData;
+};
 
 const filterData = (data) => {
   let filteredData = [];
@@ -102,7 +87,9 @@ const filterData = (data) => {
     }
   });
 
-  return filteredData;
+  const uniqueData = removeDuplicates(filteredData, "email");
+  //   console.log(uniqueData);
+  return uniqueData;
 };
 
 // const saveToExcel = (data) => {
@@ -111,6 +98,56 @@ const filterData = (data) => {
 //   XLSX.utils.book_append_sheet(workbook, worksheet, "Exhibitors");
 //   XLSX.writeFile(workbook, "exhibitors.xlsx");
 // };
+
+const appendToExcel = (data, filePath) => {
+  let workbook;
+  const sheetName = "Exhibitors";
+  let worksheet;
+
+  if (fs.existsSync(filePath)) {
+    // If the file exists, read the existing workbook
+    workbook = XLSX.readFile(filePath);
+    worksheet = workbook.Sheets[sheetName];
+    let existingData = [];
+
+    if (worksheet) {
+      // If the sheet exists, append new data to existing data
+      const existingData = XLSX.utils.sheet_to_json(worksheet);
+    }
+    data = [...existingData, ...data];
+  } else {
+    // If the file doesn't exist, create a new workbook and sheet
+    workbook = XLSX.utils.book_new();
+  }
+
+  // Create a new worksheet with the combined data
+  worksheet = XLSX.utils.json_to_sheet(data, {
+    header: [
+      "companyName",
+      "contactPerson",
+      "designation",
+      "country",
+      "email",
+      "website",
+      "telephone",
+      "mobile",
+      "address",
+      "hall",
+      "booth",
+    ],
+  });
+
+  // Append the worksheet to the workbook
+  //   XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
+  if (workbook.SheetNames.includes(sheetName)) {
+    workbook.Sheets[sheetName] = worksheet;
+  } else {
+    XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
+  }
+
+  // Write the workbook to the file
+  XLSX.writeFile(workbook, filePath);
+};
 
 const saveToExcel = (data) => {
   const worksheet = XLSX.utils.json_to_sheet(data, {
@@ -130,7 +167,7 @@ const saveToExcel = (data) => {
   });
   const workbook = XLSX.utils.book_new();
   XLSX.utils.book_append_sheet(workbook, worksheet, "Exhibitors");
-  XLSX.writeFile(workbook, "exhibitors.xlsx");
+  XLSX.writeFile(workbook, "exhibitors1.xlsx");
 };
 
 const main = async () => {
@@ -139,7 +176,8 @@ const main = async () => {
     const filteredData = filterData(data);
     // console.log(filteredData);
 
-    saveToExcel(filteredData);
+    // saveToExcel(filteredData);
+    appendToExcel(filteredData, "exhibitors.xlsx");
     console.log("Data saved to exhibitors.xlsx");
   }
 };
